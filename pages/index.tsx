d6473692a5e6bbb664de799458d131890f8a8d48
Boris Arkenaar <boris@arkenaar.com>
2022-10-17 10:32:33 +0100

    Update index styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div className="grid justify-center items-center h-screen space-y-20">
-      <div className="space-y-20">
-        <h1>Eerlijke WOZ</h1>
-        <a href="/start">Start de WOZ check</a>
+      <div className="space-y-10">
+        <h1 className="text-2xl">Eerlijke WOZ</h1>
+        <a
+          href="/start"
+          className="flex items-center h-12 bg-sky-600 text-white rounded-md px-4 drop-shadow"
+        >
+          Start de WOZ check
+        </a>
       </div>
     </div>
   );
